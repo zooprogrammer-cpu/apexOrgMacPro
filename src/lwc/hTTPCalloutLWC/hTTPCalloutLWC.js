9d7809ff1045365b2b8160078eef02a9b67b9ac6
AshB <ashbasn@gmaill.com>
2024-02-05 17:02:21 -0600

    LWC for httpCallout using apex controller

--- src/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
+++ src/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
@@ -25,49 +25,78 @@ export default class HTTPCalloutLWC extends LightningElement {
         this.toCurrencyValue = event.detail.value;
         console.log('this.toCurrencyValue =>', this.toCurrencyValue);
     }
+    // Using client side to make http callout
+    // handleCurrencyConversion() {
+    //     // rest api call
+    //     const queryValue = `function=CURRENCY_EXCHANGE_RATE&from_currency=${this.fromCurrencyValue}&to_currency=${this.toCurrencyValue}`;
+    //     const API_KEY =  `ENNH2GLEOOFFK1V6`;
+    //     // fetch('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=ENNH2GLEOOFFK1V6',
+    //     fetch(`https://www.alphavantage.co/query?${queryValue}&apikey=${API_KEY}`,
+    //     {
+    //         method:"GET",
+    //         headers : {
+    //             "Content-type" : "application/json",
+    //             "Authorization" : "OAuth 00D4x000008ZPFe!AQsAQEwHUiz4JEHbl4XwXA8tYZkfVRdIfIfIsZsGvDXfya_3.BbkqRVrBXVFWrrnLWmWBPe99dQKQe0eRRyX2tMoTl_YorpI\n"
+    //             }
+    //             // we get a response
+    //     }).then((response) => {
+    //         // we turn that response to json format
+    //         return response.json();
+    //     }).then((jsonResponse)=>{
+    //         // use that jsonResponse to create an object
+    //         let objData = {
+    //             From_Currency_Name : '',
+    //             From_Currency_Code : '',
+    //             To_Currency_Name : '',
+    //             To_Currency_Code : '',
+    //             Last_Refreshed : '',
+    //             Exchange_rate : ''
+    //         };
+    //         window.console.log('jsonresponse ==>' , JSON.stringify(jsonResponse));
+    //         let exchangeData = jsonResponse['Realtime Currency Exchange Rate'];
+    //         window.console.log('exchangeData ==>' + JSON.stringify(exchangeData));
+    //
+    //         objData.From_Currency_Code=exchangeData['1. From_Currency Code'];
+    //         objData.From_Currency_Name=exchangeData['2. From_Currency Name'];
+    //         objData.To_Currency_Name=exchangeData['4. To_Currency Name'];
+    //         objData.To_Currency_Code=exchangeData['3. To_Currency Code'];
+    //         objData.Last_Refreshed=exchangeData['6. Last Refreshed'];
+    //         objData.Exchange_rate=exchangeData['5. Exchange Rate'];
+    //         this.conversionData=objData;
+    //         window.console.log('objData => '+JSON.stringify(objData));
+    //
+    //
+    //     }).catch((error)=>{
+    //         window.console.log('callout error' ,JSON.stringify(error));
+    //     })
+    // }
 
-    handleCurrencyConversion() {
+    // Using server side apex controller to make callout:
+    async handleCurrencyConversion() {
         // rest api call
         const queryValue = `function=CURRENCY_EXCHANGE_RATE&from_currency=${this.fromCurrencyValue}&to_currency=${this.toCurrencyValue}`;
         const API_KEY =  `ENNH2GLEOOFFK1V6`;
-        // fetch('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=ENNH2GLEOOFFK1V6',
-        fetch(`https://www.alphavantage.co/query?${queryValue}&apikey=${API_KEY}`,
-        {
-            method:"GET",
-            headers : {
-                "Content-type" : "application/json",
-                "Authorization" : "OAuth 00D4x000008ZPFe!AQsAQEwHUiz4JEHbl4XwXA8tYZkfVRdIfIfIsZsGvDXfya_3.BbkqRVrBXVFWrrnLWmWBPe99dQKQe0eRRyX2tMoTl_YorpI\n"
-                }
-                // we get a response
-        }).then((response) => {
-            // we turn that response to json format
-            return response.json();
-        }).then((jsonResponse)=>{
-            // use that jsonResponse to create an object
-            let objData = {
-                From_Currency_Name : '',
-                From_Currency_Code : '',
-                To_Currency_Name : '',
-                To_Currency_Code : '',
-                Last_Refreshed : '',
-                Exchange_rate : ''
-            };
-            window.console.log('jsonresponse ==>' , JSON.stringify(jsonResponse));
-            let exchangeData = jsonResponse['Realtime Currency Exchange Rate'];
-            window.console.log('exchangeData ==>' + JSON.stringify(exchangeData));
+        const endPoint = `https://www.alphavantage.co/query?${queryValue}&apikey=${API_KEY}`;
+        const currencyDataResult = await getCurrencyData({strEndPointURL : endPoint});
+        console.log(currencyDataResult);
+        const exchangeData = currencyDataResult['Realtime Currency Exchange Rate'];
 
-            objData.From_Currency_Code=exchangeData['1. From_Currency Code'];
-            objData.From_Currency_Name=exchangeData['2. From_Currency Name'];
-            objData.To_Currency_Name=exchangeData['4. To_Currency Name'];
-            objData.To_Currency_Code=exchangeData['3. To_Currency Code'];
-            objData.Last_Refreshed=exchangeData['6. Last Refreshed'];
-            objData.Exchange_rate=exchangeData['5. Exchange Rate'];
-            this.conversionData=objData;
-            window.console.log('objData => '+JSON.stringify(objData));
+        let objData = {
+            From_Currency_Name : '',
+            From_Currency_Code : '',
+            To_Currency_Name : '',
+            To_Currency_Code : '',
+            Last_Refreshed : '',
+            Exchange_rate : ''
+        };
 
-
-        }).catch((error)=>{
-            window.console.log('callout error' ,JSON.stringify(error));
-        })
+        objData.From_Currency_Code=exchangeData['1. From_Currency Code'];
+        objData.From_Currency_Name=exchangeData['2. From_Currency Name'];
+        objData.To_Currency_Name=exchangeData['4. To_Currency Name'];
+        objData.To_Currency_Code=exchangeData['3. To_Currency Code'];
+        objData.Last_Refreshed=exchangeData['6. Last Refreshed'];
+        objData.Exchange_rate=exchangeData['5. Exchange Rate'];
+        this.conversionData=objData;
     }
+
 }
