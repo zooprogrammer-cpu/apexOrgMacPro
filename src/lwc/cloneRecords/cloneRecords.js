c2992e6cf1b648cbe00df58ba45265bfa4457852
AshB <ashbasn@gmaill.com>
2024-03-16 19:30:19 -0500

    Interface for Cloning different Objects

---
+++ src/lwc/cloneRecords/cloneRecords.js
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+
+import {LightningElement, api} from 'lwc';
+import cloneRecordsController from '@salesforce/apex/CloneRecordsController.cloneRecords';
+import cloneRelatedRecordsController from '@salesforce/apex/CloneRecordsController.cloneRelatedRecords'
+export default class CloneRecords extends LightningElement {
+  @api recordId;
+  @api className;
+
+  handleCloneRecord() {
+    cloneRelatedRecordsController({"recordId" :this.recordId, "className" : this.className}).then(()=>{
+      console.log('Cloned Record Success')
+      this.handleCloneRelatedRecord();
+    })
+
+  }
+
+  handleCloneRelatedRecord() {
+    cloneRelatedRecordsController({"recordId": this.recordId, "className": this.className}).then(() => {
+      console.log('Cloned Related Record Success')
+    })
+  }
+
+}
