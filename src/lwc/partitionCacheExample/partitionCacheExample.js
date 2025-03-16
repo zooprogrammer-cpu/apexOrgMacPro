e8fe5deb543b391113d524f9d51b1c334a23397a
ashbas <ashbasn@gmail.com>
2025-03-16 16:28:51 -0500

    Retrieved all

--- src/lwc/partitionCacheExample/partitionCacheExample.js
+++ src/lwc/partitionCacheExample/partitionCacheExample.js
@@ -2,22 +2,32 @@
  * Created by ashbas on 3/11/25.
  */
 
-import {LightningElement, api} from 'lwc';
+import {LightningElement, api, wire} from 'lwc';
 import retrieveDataCachedController from '@salesforce/apex/PartitionCacheExample.retrieveDataCachedController';
 import storeDataController from '@salesforce/apex/PartitionCacheExample.storeDataController';
 import retrieveDataSOQLController from '@salesforce/apex/PartitionCacheExample.retrieveDataSOQLController';
+import {CurrentPageReference} from "lightning/navigation";
 
 
 export default class PartitionCacheExample extends LightningElement {
  @api recordId;
   accountId;
+  currentPageReference;
 
   connectedCallback() {
     this.storeData();
   }
 
+  @wire(CurrentPageReference)
+  setCurrentPageReference(currentPageReference) {
+    this.currentPageReference = currentPageReference;
+    this.recordId = this.currentPageReference.attributes.recordId;
+  }
+
+
   storeData() {
-    storeDataController().then(()=> {
+    console.log('this.recordId: ', this.recordId);
+    storeDataController({recordId : this.recordId}).then(()=> {
       console.log('We stored the data');
     })
   }
@@ -30,7 +40,7 @@ export default class PartitionCacheExample extends LightningElement {
   }
 
   retrieveDataSOQL() {
-    retrieveDataSOQLController().then(data=> {
+    retrieveDataSOQLController({recordId : this.recordId}).then(data=> {
       console.log('This is the data from the SOQL: ', data);
       this.accountId = data.AccountId
     });
